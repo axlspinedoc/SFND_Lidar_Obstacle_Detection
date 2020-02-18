afefa7d8da0f9e635e17a7c0cfff4dbc002baccd
axlspinedoc <axel_robot@hotmail.com>
2020-02-18 12:19:19 -0600

    saving personal changes of course

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,12 +42,19 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
-    // TODO:: Create lidar sensor 
-
+    // TODO:: Create lidar sensor
+    Lidar* lidar = new Lidar(cars,0); 
+    
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
+    
+    //renderRays(viewer,lidar->position,inputCloud);
+    renderPointCloud(viewer,inputCloud,"inputCloud");
+    
     // TODO:: Create point processor
+
   
 }
 
@@ -70,9 +77,10 @@ void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& vi
         case Side : viewer->setCameraPosition(0, -distance, 0, 0, 0, 1); break;
         case FPS : viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
     }
-
     if(setAngle!=FPS)
         viewer->addCoordinateSystem (1.0);
+
+    ProcessPointClouds<pcl::PointXYZ>* pointProcessor = new ProcessPointClouds<pcl::PointXYZ>();
 }
 
 
--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -56,6 +56,24 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
 	pcl::PointIndices::Ptr inliers;
     // TODO:: Fill in this function to find inliers for the cloud.
 
+    pcl::SACSegmentation<PointT> seg;
+    pcl::PointIndices::Ptr inliers {new pcl::PointIndices};
+    pcl::ModelCoefficients::Ptr coefficients {new pcl::ModelCoefficients};
+
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations(maxIterations);
+    seg.setDistanceThreshold(distanceThreshold);
+
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coefficients);
+
+    if (inliers->indices.size()==0)
+    {
+        std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+    }
+
     auto endTime = std::chrono::steady_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
     std::cout << "plane segmentation took " << elapsedTime.count() << " milliseconds" << std::endl;
